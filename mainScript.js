3790264f1da95e4b8b081b057e8e8bf434a11410
UniquelyElite <rylanmetznerm@gmail.com>
2021-05-01 21:31:21 -0600

    yeetis the code boi

--- mainScript.js
+++ mainScript.js
@@ -1,24 +1,42 @@
-alert("This is a work in progress!")
+if (localStorage.system == undefined)
+{
+    alert("This is a work in progress!")
+}
+
+let systemReload = false;
 
 let text = document.getElementById("textArea");
 let prompt = document.getElementById("text");
-
-let system = {
+let initPromptText = "null@null:~$"
+let promptText = "null@null:~$"
+prompt.innerText = promptText;
+let initSystem = {
     "/": {
-        //"README.md": {},
-        //"test": {
-        //    "lol": {
-        //        "lmao": {}
-        //    }
-        //}
+        "README.md": {},
+        "test": {
+            "lol": {
+                "lmao": {}
+            }
+        }
     }
+};
+let system = initSystem
+if (localStorage.system == undefined)
+{
+    localStorage.system = JSON.stringify(system)
+}
+else
+{
+    system = JSON.parse(localStorage.system)
 }
 
 let commands = {
     "ls": {},
     "echo [text]": {},
     "mkdir [name]": {},
-    "cd [directory]": {}
+    "cd [directory]": {},
+    "cls": {},
+    "hardReset": {}
     //"newusr [name password]": {}
 }
 
@@ -42,16 +60,27 @@ function loadDirectoryFromString(str)
         })
         if (currentDir == undefined)
         {
-            currentDir = lastDir
-            currentDirStr = lastDirStr
             prompt.innerText += "\nERROR:";
             text.innerText += `\nThe directory '${currentDirStr}' does not exist`
+            currentDir = lastDir
+            currentDirStr = lastDirStr
+        }
+        else
+        {
+            promptText = initPromptText.split("$")[0] + `${currentDirStr}$`
         }
         return;
     }
     else if (element == "..")
     {
         currentDirStr = currentDirStr.split("/").slice(0, -1).join("/")
+    }
+    else if (element.includes("."))
+    {
+        //its a openable file
+        file = element.split(".")[0]
+        readTextFile(`${file}.txt`)
+
     }
     else
     {
@@ -67,8 +96,16 @@ window.addEventListener("keydown", function(event){
         let command = unfiltered[0];
         let information = unfiltered.splice(1);
         commandSorter(command, information);
-        text.innerText += "\n";
-        prompt.innerText += "\nnull@null:~$";
+        if (prompt.innerText != "")
+        {
+            prompt.innerText += `\n${promptText}`;
+            text.innerText += "\n";
+        }
+        else
+        {
+            prompt.innerText = promptText;
+            text.innerText = "";
+        }
     } else if (event.key == "Shift" || event.key == "Control" || event.key == "Undefined" ){
 
     } else if (event.key == "Backspace") {
@@ -115,7 +152,7 @@ function help(command)
 {
     if (command != "")
     {
-        //console.log("not empty")
+        //console.log("not empty") #wip
 
     } else {
         //console.log("empty")
@@ -131,7 +168,7 @@ function help(command)
 
 function test(lol){console.log(lol)}
 
-function ls()
+ls = dir = function()
 {
     prompt.innerText += "\nDirectory:"
     text.innerText += "\n"
@@ -154,8 +191,33 @@ function mkdir(name)
     }
 }
 
-function open(name)
+function cls()
 {
-    //wip
-
+    text.innerText = "";
+    prompt.innerText = "";
 }
+
+function hardReset()
+{
+    localStorage.clear();
+    system = initSystem;
+    systemReload = true;
+    location.reload()
+}
+
+
+
+
+
+//OTHER STUFF
+window.addEventListener("beforeunload", function (e) {
+    if (!systemReload)
+    {
+        localStorage.system = JSON.stringify(system);
+        var confirmationMessage = "Dont Leave T-T";
+    
+        (e || window.event).returnValue = confirmationMessage;
+        return confirmationMessage;
+    }
+  
+});
